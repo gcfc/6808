613c03b554336bc29434fa0d275025c88aebf28f
Daniel Kuang <dk6548@mit.edu>
2021-04-24 22:37:05 -0400

    integrated Daniil-Osokin's lightweight human-pose-estimation and modularized repo

---
+++ main.py
@@ -0,0 +1,101 @@
+import argparse
+
+import cv2
+import numpy as np
+import torch
+import time
+
+from models.with_mobilenet import PoseEstimationWithMobileNet
+from modules.load_state import load_state
+from modules.utils import Graph
+import matplotlib.pyplot as plt
+from demo import run_demo
+
+### NOTES FOR TEAM 1 ###
+
+## INSTRUCTIONS
+
+# All comments elsewhere besides this file must NOT be deleted without every team member's approval
+# Lines of code that are added by TEAM 1 in other files MUST be marked with "#TEAM" at the end of the line
+# For any other function that you will define and use in main(), please implement in modules/utils.py and import the function here
+
+
+## HOW TO START PROGRAM
+
+# If you would like to test with VIDEO:
+# python main.py --checkpoint-path <path_to>/checkpoint_iter_370000.pth --video 0
+# example: python main.py --checkpoint-path ./models/checkpoint_iter_370000.pth --video 0
+
+# If you would like to test with IMAGES:
+# python main.py --checkpoint-path <path_to>/checkpoint_iter_370000.pth --images <path_to_image>
+# example: python main.py --checkpoint-path ./models/checkpoint_iter_370000.pth --images images/test_img.jpg
+
+
+## REFERENCE
+
+# The following are supported joints from ML. The indx of an item is its body part id
+# kpt_names = ['nose', 'neck',
+#                 'r_sho', 'r_elb', 'r_wri', 'l_sho', 'l_elb', 'l_wri',
+#                 'r_hip', 'r_knee', 'r_ank', 'l_hip', 'l_knee', 'l_ank',
+#                 'r_eye', 'l_eye',
+#                 'r_ear', 'l_ear']
+
+def main():
+    #graph is also delayed by the same value, so 2*delay is the actual delay time. why twice: must delay graph or else pyplot freezes
+    delay = 0.001
+
+    # variable for video camera
+    cam = None
+
+    #initializing graph
+    fig = plt.figure()
+    map_ax = fig.add_subplot(111, projection="3d")
+    map_ax.autoscale(enable=True, axis='both', tight=True)
+    graph = Graph(ax=map_ax, coords=[], delay=delay)
+
+    while True:
+        # TODO get data from IMUs
+
+        # pass webcam pic into analyzer to get body pose points
+        if args.video != '':
+            if cam is None:
+                cam = cv2.VideoCapture(args.video)
+            was_read, img = cam.read()
+            if not was_read or not cam.isOpened():
+                print("Camera is either closed or missing.")
+                break
+        else:
+            # only analyze first image of given list of images
+            img = cv2.imread(args.images[0], cv2.IMREAD_COLOR)
+        frame_provider = [img]
+        points = run_demo(net, frame_provider, args.height_size, args.cpu, args.track, args.smooth)
+
+        # TODO invoke utility functions to calculate final body pose points
+
+        # plot calculated points on animated 3D graph
+        # graph.update_coords(coords)
+        # graph.plot()
+        # graph.show()
+
+        time.sleep(delay)
+
+
+# DO NOT EDIT THE FOLLOWING CODE, EDIT ONLY IN main()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='''Team 1 Body Pose Tracking with IMUs''')
+    parser.add_argument('--checkpoint-path', type=str, required=True, help='path to the checkpoint')
+    parser.add_argument('--height-size', type=int, default=256, help='network input layer height size')
+    parser.add_argument('--video', type=str, default='', help='path to video file or camera id')
+    parser.add_argument('--images', nargs='+', default='', help='path to input image(s)')
+    parser.add_argument('--cpu', action='store_true', help='run network inference on cpu')
+    parser.add_argument('--track', type=int, default=1, help='track pose id in video')
+    parser.add_argument('--smooth', type=int, default=1, help='smooth pose keypoints')
+    args = parser.parse_args()
+
+    net = PoseEstimationWithMobileNet()
+    checkpoint = torch.load(args.checkpoint_path, map_location='cpu')
+    load_state(net, checkpoint)
+
+    main()
+
---
+++ modules/utils.py
@@ -0,0 +1,71 @@
+# TEAM 1 utils.py
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.style as style
+
+style.use("ggplot")
+body_parts = ['nose', 'neck',
+              'r_sho', 'r_elb', 'r_wri', 'l_sho', 'l_elb', 'l_wri',
+              'r_hip', 'r_knee', 'r_ank', 'l_hip', 'l_knee', 'l_ank',
+              'r_eye', 'l_eye',
+              'r_ear', 'l_ear']
+
+
+class Graph():
+    def __init__(self, ax, coords, delay):
+        self.ax = ax
+        # delay: in seconds
+        self.delay = delay
+        # coords should be a list of length 3 lists
+        # each sublist should correspond to the correct body part
+        self.x, self.y, self.z = self.update_coords(coords)
+        self.id_p = {
+            body_parts[i]: i for i in range(body_parts)
+        }
+
+    def plot(self):
+        self.ax.clear()
+        self.ax.scatter(self.x, self.y, self.z, s=100, label="Body Pose Position")
+        self.draw()
+
+    def draw(self):
+        self.trace("nose", "neck")
+
+        self.trace("neck", "r_sho")
+        self.trace("r_sho", "r_elb")
+        self.trace("r_elb", "r_wri")
+
+        self.trace("neck", "r_hip")
+        self.trace("r_hip", "r_knee")
+        self.trace("r_knee", "r_ank")
+
+        self.trace("neck", "l_sho")
+        self.trace("l_sho", "l_elb")
+        self.trace("l_elb", "l_wri")
+
+        self.trace("neck", "l_hip")
+        self.trace("l_hip", "l_knee")
+        self.trace("l_knee", "l_ank")
+
+    def update_coords(self, coords):
+        x, y, z = [], [], []
+        if len(coords) == 0:
+            return x,y,z
+        if len(coords[0]) != 3:
+            print("Coords must be length 3")
+            return x,y,z
+        coords = list(zip(*coords))
+        x = np.array(coords[0], dtype=np.float)
+        y = np.array(coords[1], dtype=np.float)
+        z = np.array(coords[2], dtype=np.float)
+        return x,y,z
+
+    def trace(self, part1, part2):
+        self.ax.plot3D([self.x[self.id_p[part1]], self.x[self.id_p[part2]],
+                self.y[self.id_p[part1]], self.y[self.id_p[part2]],
+                self.z[self.id_p[part1]], self.z[self.id_p[part2]]])
+
+    def show(self):
+        plt.show(block=False)
+        plt.pause(self.delay)
