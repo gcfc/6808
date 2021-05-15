210d60f091f8e3120c1f37fedc5625012bc0622a
George Chen <georgechen1999@gmail.com>
2021-05-15 02:20:12 -0700

    two more sensors

--- pose/get_data/get_data.ino
+++ pose/get_data/get_data.ino
@@ -27,31 +27,31 @@ Adafruit_BNO055 bno  = Adafruit_BNO055(55, 0x28);
 Adafruit_BNO055 bno2 = Adafruit_BNO055(55, 0x29);
 
 // constant resources
-const double pi        = 3.141592653589793238462643383;
-const float  L1        = 0.225; // shoulder to elbow (m)
-const float  L2        = 0.25;  // elbow to wrist (m)
-const int    num_buses = 2;     // currently only using SD/SC 0 and 1
+const double pi                = 3.141592653589793238462643383;
+const float  L_arm             = 0.225; // shoulder to elbow, or elbow to wrist (m)
+const int    num_buses         = 2;     // currently only using SD/SC 0 and 1
+const int    num_total_sensors = 11;
 
 // variable declarations
 int num_sensors;
 float eul_x, eul_z;
 
-float x[11],  y[11],  z[11];    // = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-float dx[11], dy[11], dz[11];
+float x[num_total_sensors],   y[num_total_sensors],  z[num_total_sensors];    // = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+float dx[num_total_sensors], dy[num_total_sensors], dz[num_total_sensors];
 
-float phi[11],     phi_prev[11],   phi_offset[11];
-float theta[11], theta_prev[11], theta_offset[11];
+float   phi[num_total_sensors],   phi_prev[num_total_sensors],   phi_offset[num_total_sensors];
+float theta[num_total_sensors], theta_prev[num_total_sensors], theta_offset[num_total_sensors];
 
-bool calibrated[11];
-int negate_x[11] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}; // either 1 or -1
-int negate_y[11] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}; // either 1 or -1
-char out[500];
+bool calibrated[num_total_sensors];
+int    negate_x[num_total_sensors] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}; // either 1 or -1
+int    negate_y[num_total_sensors] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}; // either 1 or -1
+char   out[500];
 
-bool validating[11], validated[11];
-int validate_timer;
-float init_x[11], init_y[11];
-float min_x[11], max_x[11], min_y[11], max_y[11];
-bool printed;
+bool  validating[num_total_sensors], validated[num_total_sensors];
+int   validate_timer;
+float init_x[num_total_sensors], init_y[num_total_sensors];
+float  min_x[num_total_sensors],  max_x[num_total_sensors],  min_y[num_total_sensors],  max_y[num_total_sensors];
+bool  printed;
 
 void setup()
 {
@@ -79,6 +79,15 @@ void setup()
   Serial.println("Orientation Sensor Test begins...");
 
   for (uint8_t t = 0; t < num_buses; t++) {
+
+    int i = t + 2;
+    if (t == 0) {
+      i = t;
+    }
+    else if (t == 1) {
+      i = t + 1;
+    }
+
     tcaselect(t);
     /* Initialise the sensor */
     if (!bno.begin())
@@ -87,8 +96,19 @@ void setup()
       Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
       while (1);
     }
+    Serial.print("Orientation Sensor Test complete: "); Serial.println(i);
 
-    Serial.print("Orientation Sensor Test complete: "); Serial.println(t + 1);
+    if (t == 0 || t == 1) {
+      i++;
+      /* Initialise the sensor */
+      if (!bno2.begin())
+      {
+        /* There was a problem detecting the BNO055 ... check your connections */
+        Serial.print("Ooops, no BNO055 at 0x29 detected ... Check your wiring or I2C ADDR at 0x29!");
+        while (1);
+      }
+      Serial.print("Orientation Sensor Test complete: "); Serial.println(i);
+    }
   }
   delay(1000);
 
@@ -98,68 +118,23 @@ void setup()
 void loop()
 {
   Serial.print(micros()); Serial.print("\t");
-  for (uint8_t t = 0; t < num_sensors; t++) {
-    tcaselect(t);
+  for (uint8_t b = 0; b < num_buses; b++) {
+    tcaselect(b);
 
-    /* Get a new sensor event */
-    sensors_event_t event;
-    bno.getEvent(&event);
-
-    eul_x = event.orientation.x;    // deg
-    eul_z = event.orientation.z;    // deg
-    //    Serial.print((int)eul_x); Serial.print("\t"); Serial.print((int)eul_z); Serial.print("\t");
-
-    // angles in the rest of the code are in radians
-    theta[t] =  eul_z * pi / 180.0;//- theta_offset[t];
-    phi[t]   =  eul_x * pi / 180.0;//- theta_offset[t];
-
-    // if not calibrated, calibrate and set angles = 0
-    uint8_t system, gyro, accel, mag = 0;
-    bno.getCalibration(&system, &gyro, &accel, &mag);
-    if (!calibrated[t]) {
-      if (mag == 3) {
-        phi_offset[t] = phi[t];
-        theta_offset[t] = theta[t];
-        calibrated[t] = true;
-        sprintf(out, "Sensor %d calibrated successfully!", t);
-        Serial.println(out);
-      }
-      else {
-        printCalibrationStatus();
-      }
+    // internal variables: b - buses, i - sensors
+    int i = b + 2;
+    if (b == 0) {
+      i = b;
+    }
+    else if (b == 1) {
+      i = b + 1;
     }
 
-    if (all_calibrated()) {
-      dx[t] = negate_x[t] * (L1 * sin(phi[t]) * cos(theta[t]) - L1 * sin(phi_prev[t]) * cos(theta_prev[t])) ;
-      dy[t] = negate_y[t] * (L1 * sin(phi[t]) * sin(theta[t]) - L1 * sin(phi_prev[t]) * sin(theta_prev[t])) ;
-      dz[t] = -(L1 * cos(phi[t]) - L1 * cos(phi_prev[t]));
-      x[t] += dx[t]; y[t] += dy[t]; z[t] += dz[t];
-      theta_prev[t] = theta[t];
-      phi_prev[t] = phi[t];
+    sensorOperations(&bno, i);
 
-      //        Serial.print(dx[t]); Serial.print("\t"); Serial.print(dy[t]); Serial.print("\t"); Serial.print(dz[t]); Serial.print("\t \t");
-      Serial.print(x[t]); Serial.print("\t"); Serial.print(y[t]); Serial.print("\t"); Serial.print(z[t]); Serial.print("\t \t");
-
-      // janky validation code, wave forearm only
-      if (!validated[t]) {
-        if (!validating[t]) {
-          validate_timer = millis();
-          init_x[t] = x[t];
-          init_y[t] = y[t];
-          validating[t] = true;
-        }
-        if (millis() - validate_timer < 10000) {
-          min_x[t] = (x[t] < min_x[t]) ? x[t] : min_x[t];
-          min_y[t] = (y[t] < min_y[t]) ? y[t] : min_y[t];
-          max_x[t] = (x[t] > max_x[t]) ? x[t] : max_x[t];
-          max_y[t] = (x[t] > max_y[t]) ? y[t] : max_y[t];
-        }
-        else {
-          negate_x[t] = (abs(init_x[t] - min_x[t]) > abs(init_x[t] - max_x[t])) ? 1 : -1;
-//          negate_y[t] = (abs(init_y[t] - max_y[t]) > abs(init_y[t] - min_y[t])) ? 1 : -1;
-          validated[t] = true;
-        }
-      }
+    if (b == 0 || b == 1) {
+      i++;
+      sensorOperations(&bno2, i);
     }
 
   }
@@ -173,7 +148,7 @@ void loop()
     Serial.print(negate_x[1]); Serial.print("\t"); Serial.println(negate_y[1]);
     Serial.print(init_x[1]); Serial.print("\t"); Serial.print(min_x[1]); Serial.print("\t"); Serial.println(max_x[1]);
     Serial.print(init_y[1]); Serial.print("\t"); Serial.print(min_y[1]); Serial.print("\t"); Serial.println(max_y[1]);
-    delay(20000);
+    delay(1000);
     printed = true;
     Serial.println("\n=========== ALL VALIDATED! ===========");
   }
--- pose/get_data/helper_functions.ino
+++ pose/get_data/helper_functions.ino
@@ -47,52 +47,66 @@ void printCalibrationStatus() {
   Serial.println();
 }
 
-void printEvent(sensors_event_t* event) {
-  double x = -1000000, y = -1000000 , z = -1000000; //dumb values, easy to spot problem
-  if (event->type == SENSOR_TYPE_ACCELEROMETER) {
-    Serial.print("Accl:");
-    x = event->acceleration.x;
-    y = event->acceleration.y;
-    z = event->acceleration.z;
-  }
-  else if (event->type == SENSOR_TYPE_ORIENTATION) {
-    Serial.print("Orient:");
-    x = event->orientation.x;
-    y = event->orientation.y;
-    z = event->orientation.z;
-  }
-  else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
-    Serial.print("Mag:");
-    x = event->magnetic.x;
-    y = event->magnetic.y;
-    z = event->magnetic.z;
-  }
-  else if (event->type == SENSOR_TYPE_GYROSCOPE) {
-    Serial.print("Gyro:");
-    x = event->gyro.x;
-    y = event->gyro.y;
-    z = event->gyro.z;
-  }
-  else if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
-    Serial.print("Rot:");
-    x = event->gyro.x;
-    y = event->gyro.y;
-    z = event->gyro.z;
-  }
-  else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
-    Serial.print("Linear:");
-    x = event->acceleration.x;
-    y = event->acceleration.y;
-    z = event->acceleration.z;
-  }
-  else {
-    Serial.print("Unk:");
-  }
+void sensorOperations(Adafruit_BNO055* my_bno, int i) {
+  /* Get a new sensor event */
+    sensors_event_t event;
+    my_bno->getEvent(&event);
+    uint8_t system, gyro, accel, mag = 0;
+    my_bno->getCalibration(&system, &gyro, &accel, &mag);
 
-  Serial.print("\tx= ");
-  Serial.print(x);
-  Serial.print(" |\ty= ");
-  Serial.print(y);
-  Serial.print(" |\tz= ");
-  Serial.print(z);
+    eul_x = event.orientation.x;    // deg
+    eul_z = event.orientation.z;    // deg
+    //    Serial.print((int)eul_x); Serial.print("\t"); Serial.print((int)eul_z); Serial.print("\t");
+
+
+    // angles in the rest of the code are in radians
+    theta[i] =  eul_z * pi / 180.0;//- theta_offset[i];
+    phi[i]   =  eul_x * pi / 180.0;//- theta_offset[i];
+
+    // if not calibrated, calibrate and set angles = 0
+    if (!calibrated[i]) {
+      if (mag == 3) {
+        phi_offset[i] = phi[i];
+        theta_offset[i] = theta[i];
+        calibrated[i] = true;
+        sprintf(out, "Sensor %d calibrated successfully!", i);
+        Serial.println(out);
+      }
+      else {
+        printCalibrationStatus();
+      }
+    }
+
+    if (all_calibrated()) {
+      dx[i] = negate_x[i] * (L_arm * sin(phi[i]) * cos(theta[i]) - L_arm * sin(phi_prev[i]) * cos(theta_prev[i])) ;
+      dy[i] = negate_y[i] * (L_arm * sin(phi[i]) * sin(theta[i]) - L_arm * sin(phi_prev[i]) * sin(theta_prev[i])) ;
+      dz[i] = -(L_arm * cos(phi[i]) - L_arm * cos(phi_prev[i]));
+      x[i] += dx[i]; y[i] += dy[i]; z[i] += dz[i];
+      theta_prev[i] = theta[i];
+      phi_prev[i] = phi[i];
+
+      //        Serial.print(dx[i]); Serial.print("\t"); Serial.print(dy[i]); Serial.print("\t"); Serial.print(dz[i]); Serial.print("\t \t");
+      Serial.print(x[i]); Serial.print("\t"); Serial.print(y[i]); Serial.print("\t"); Serial.print(z[i]); Serial.print("\t \t");
+
+      // janky validation code, raise arm to determine negation sign
+      if (!validated[i]) {
+        if (!validating[i]) {
+          validate_timer = millis();
+          init_x[i] = x[i];
+          init_y[i] = y[i];
+          validating[i] = true;
+        }
+        if (millis() - validate_timer < 10000) {
+          min_x[i] = (x[i] < min_x[i]) ? x[i] : min_x[i];
+          min_y[i] = (y[i] < min_y[i]) ? y[i] : min_y[i];
+          max_x[i] = (x[i] > max_x[i]) ? x[i] : max_x[i];
+          max_y[i] = (x[i] > max_y[i]) ? y[i] : max_y[i];
+        }
+        else {
+          negate_x[i] = (abs(init_x[i] - min_x[i]) > abs(init_x[i] - max_x[i])) ? 1 : -1;
+//          negate_y[i] = (abs(init_y[i] - max_y[i]) > abs(init_y[i] - min_y[i])) ? 1 : -1; // if i == 1, negate_y[i] = 1
+          validated[i] = true;
+        }
+      }
+    }
 }
